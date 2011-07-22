9e34fdb4dd0c3b62664eeb8ad9f59ea6cec1c533
Ismo Vuorinen <ivuorinen@gmail.com>
2011-07-22 16:16:28 +0300

    Clean-up

--- example.php
+++ example.php
@@ -1,16 +1,16 @@
 <?php
 #header('Content-Type: text/calendar; charset=utf-8');
-error_reporting(E_ALL ^ E_NOTICE);
+#error_reporting(E_ALL ^ E_NOTICE);
 date_default_timezone_set('Europe/Helsinki');
 
 require_once("simple_html_dom.php");
 require_once("bassofeed.php");
 
 #echo "<pre>";
-$basso_alas = new BassoFeed('alas');
-echo $basso_alas->get_ical();
+#$basso_alas = new BassoFeed('alas');
+#echo $basso_alas->get_ical();
 
-echo "\n----------------------------\n\n\n\n";
+#echo "\n----------------------------\n\n\n\n";
 
 $basso_multiple = new BassoFeed(array('alas', 'darkdays'));
 echo $basso_multiple->get_ical();
